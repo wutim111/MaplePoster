6a555acb105b7089ba3af6111477f19431048e96
wutim111 <wutim111@gmail.com>
2023-08-30 08:57:33 +0800

    1

--- js/myScript.js
+++ js/myScript.js
@@ -2,9 +2,9 @@
 window.onload = function () {
     let Btn = document.getElementById('btn');
     Btn.onclick = function () {
-        let new_window=window.open('https://www.facebook.com/permalink.php?story_fbid=pfbid02DXgBScTu3ARkWEMWiZhtQT7Sf7hFZvidW7trSUGmvA6PAxRLzQgrDg3Q7VDNxw1ml&id=100000454267629');
-        new_window.onload=function(){
-            
-        }
+       // let new_window=window.open('https://www.facebook.com/permalink.php?story_fbid=pfbid02DXgBScTu3ARkWEMWiZhtQT7Sf7hFZvidW7trSUGmvA6PAxRLzQgrDg3Q7VDNxw1ml&id=100000454267629');
+        //new_window.onload=function(){
+        //    
+        //}
     }
 }
