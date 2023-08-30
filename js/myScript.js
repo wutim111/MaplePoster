0ae9d24ae7b634d1756bcdd33822aeda982b2deb
wutim111 <wutim111@gmail.com>
2023-08-30 08:52:30 +0800

    1

--- js/myScript.js
+++ js/myScript.js
@@ -1,5 +1,10 @@
 
-
-function check(){
-
+window.onload = function () {
+    let Btn = document.getElementById('btn');
+    Btn.onclick = function () {
+        let new_window=window.open('https://www.facebook.com/permalink.php?story_fbid=pfbid02DXgBScTu3ARkWEMWiZhtQT7Sf7hFZvidW7trSUGmvA6PAxRLzQgrDg3Q7VDNxw1ml&id=100000454267629');
+        new_window.onload=function(){
+            
+        }
+    }
 }
